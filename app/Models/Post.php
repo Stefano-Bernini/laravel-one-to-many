c68b56716957e79df7cfcd6976314aeb6d07512c
Stefano-Bernini <stefanobernini26@gmail.com>
2023-09-01 00:12:33 +0200

    aggiunti fillable e functions a Category e Post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,12 +6,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use Illuminate\Support\Str;
+use App\Models\Category;
 
 class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'content', 'slug', 'cover_image'];
+    protected $fillable = ['title', 'content', 'slug', 'cover_image', 'category_id'];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 
     public static function generateSlug($title)
     {
